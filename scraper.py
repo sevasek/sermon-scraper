7939d734bc57facdfbc6da460d9654604417075e
sevasek <165224435+sevasek@users.noreply.github.com>
2026-05-19 18:29:25 +1000

    Working through bugs

--- scraper.py
+++ scraper.py
@@ -16,7 +16,6 @@ def craft_results_url(passage):
     results_url = base_url.replace(keyword, passage.replace(" ", "+"))
     return results_url
 
-# INCOMPLETED FUNCTIONS
 # Input: A Playwright page object and the URL of a results page to be scraped. 
 # Each results page contains links to the sermon page.
 # The function will scrape the page for sermon page URLs and add them to the sermon_urls set.
@@ -34,33 +33,37 @@ async def scrape_one_results_page(page, results_url: str):
         print(url)
     return
 
-# Input: Takes a the URL of the first results page
-# Calls scrape_one_results_page for each results page to populate the sermon_urls set.
+# Input: Takes a the URL of the first results page.
+# Adds the URL of the first page to the results_pages set.
+# Scrapes the page for the remaining results page URLs and adds them to the results_pages set.
+# In a loop, calls scrape_one_results_page with results page URL, until all results pages have been scraped.
+# The individual sermon page URLs for each results page are caught and added to the set of individual sermon page URLs.
 # Output: Returns a list of sermon URLs.
 async def get_all_sermon_urls(start_url: str):
 
-    sermon_urls = set()
-
+    # A set for the individual sermon page links.
+    # These pages contain the mp3 links and other meta information about the sermons.
+    # This set is updated below, and will be processed by the scrape_all_sermon_page_urls function.
+    sermon_page_urls = set()
+        
+    # Starts the Playwright browser and navigates to the start URL to begin scraping.
     async def run(playwright: Playwright):
         chromium = playwright.chromium
         browser = await chromium.launch(headless=True)
         page = await browser.new_page()
         await page.goto(start_url, wait_until="domcontentloaded")
-        await page.wait_for_selector('a[href^="/media/"]', timeout=15000)
-        
-        sermon_links = await page.query_selector_all('a[href^="/media/"]')
 
-        for link in sermon_links:
-            sermon_url = "https://evchurch.info" + await link.get_attribute("href")
-            single_sermon_pages.add(sermon_url)
+        # Initialise pagination_links with the start URL and update the links to the remaining results pages.
+        pagination_links = set()
+        pagination_links.add(start_url)
+        pagination_links.update(await page.query_selector_all('a[href*="display_page="]'))
 
-        pagination_links = await page.query_selector_all('a[href*="display_page="]')
+        # Loop through the pagination links and call scrape_one_results_page for each results page URL.
         for link in pagination_links:
             href = await link.get_attribute("href")
-            if not href:
-                continue
             page_url = "https://evchurch.info" + href
-            await scrape_one_results_page(page, page_url)
+            subset_sermon_pages = list(await scrape_one_results_page(page, page_url))
+            sermon_page_urls.update(subset_sermon_pages)
 
         await browser.close()
     
@@ -72,11 +75,11 @@ async def get_all_sermon_urls(start_url: str):
     # Calls the scrape function to commence the scraping process
     asyncio.run(scrape())
 
-    return list(sermon_urls)
+    return list(sermon_page_urls)
 
 # Input: The list of sermon_urls to be scraped.
 # The scraper must identify various attributes defined in the Sermon dataclass, such as the title, speaker, date, location, bible passage, and mp3 link.
 # Output: A list of Sermon objects.
-async def scrape_all_sermon_urls(sermon_urls: list):
+async def scrape_all_sermon_page_urls(sermon_urls: list):
 
     return None
